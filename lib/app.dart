8dc04e6b74285d35d095ebb8da1405cd83041559
Daniil Novoselov <gudnmail@gmail.com>
2021-03-24 18:44:31 +0500

    add state saver

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,9 @@
+import 'dart:html';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:ddplayer/view/player_page.dart';
+import 'package:ddplayer/cubits/base_cubit.dart';
 
 class DDPlayer extends MaterialApp {
   const DDPlayer({Key? key})
@@ -7,4 +11,16 @@ class DDPlayer extends MaterialApp {
           key: key,
           home: const PlayerPage(),
         );
+}
+
+class StateSaver extends BlocObserver {
+  @override
+  void onChange(BlocBase cubit, Change change) {
+    super.onChange(cubit, change);
+    if (cubit is DDCubit) {
+      final name = cubit.state.name;
+      final value = cubit.state.value.toString();
+      window.localStorage[name] = value;
+    }
+  }
 }
--- lib/cubits/base_cubit.dart
+++ lib/cubits/base_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:bloc/bloc.dart';
 
 class KV<T> {
@@ -11,7 +13,23 @@ class DDCubit<T> extends Cubit<KV<T>> {
   final String name;
 
   DDCubit({required this.name, required T value})
-      : super(KV(name: name, value: value));
+      : super(KV(name: name, value: value)) {
+    final val = window.localStorage[name];
+    if (val != null) {
+      switch (T) {
+        case String:
+          set(val as T);
+          break;
+        case int:
+          set((int.tryParse(val) ?? 0) as T);
+          break;
+        case double:
+          set((double.tryParse(val) ?? 0) as T);
+          break;
+      }
+      window.localStorage[name] = val;
+    }
+  }
 
   void set(T value) {
     emit(KV(name: this.name, value: value));
